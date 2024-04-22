2a77f5aa181527a4a01a251ffcefb897321e3ed9
Anton Zhuravlev <abramoff.job@gmail.com>
2024-04-22 22:28:23 +0300

    Сделал кастомный компонент Text, который включает в себя заголовки, параграфы и ссылки из react-router-dom. Переделал немного цвета и размеры под rem

--- src/pages/ui/MainPage/MainPage.tsx
+++ src/pages/ui/MainPage/MainPage.tsx
@@ -1,29 +1,34 @@
-import {useState} from "react";
-import {useLoginMutation} from "@features/auth/lib";
+import { Text } from '@shared/ui';
+import { ColorEnum, SizeEnum, WeightEnum } from '@shared/lib';
 
 export const MainPage = () => {
-    const [data, setData] = useState({
-        email: "",
-        password: ""
-    })
-    const [trigger] = useLoginMutation()
     return (
         <div>
-            <input
-                value={data.email}
-                onChange={(e) => {
-                    setData({...data, email: e.target.value})
-                }}
-                type="text"/>
-            <input
-                value={data.password}
-                onChange={(e) => {
-                    setData({...data, password: e.target.value})
-                }}
-                type="text"/>
-            <button onClick={() => {
-                trigger(data)
-            }}>submit</button>
+            <Text.Heading weight={WeightEnum.BOLD} className={"1224"} size={SizeEnum.H1} color={ColorEnum.BLACK}>124</Text.Heading>
+            <Text.Heading size={SizeEnum.H2} color={ColorEnum.BLACK}>124</Text.Heading>
+            <Text.Heading size={SizeEnum.H3} color={ColorEnum.BLACK}>124</Text.Heading>
+            <Text.Heading size={SizeEnum.H4} color={ColorEnum.BLACK}>124</Text.Heading>
+            <Text.Heading size={SizeEnum.H5} color={ColorEnum.BLACK}>124</Text.Heading>
+            <Text.Heading size={SizeEnum.H6} color={ColorEnum.BLACK}>124</Text.Heading>
+
+
+
+            <Text.Paragraph size={SizeEnum.H1} color={ColorEnum.BLACK}>124</Text.Paragraph>
+            <Text.Paragraph size={SizeEnum.H2} color={ColorEnum.BLACK}>124</Text.Paragraph>
+            <Text.Paragraph size={SizeEnum.H3} color={ColorEnum.BLACK}>124</Text.Paragraph>
+            <Text.Paragraph size={SizeEnum.H4} color={ColorEnum.BLACK}>124</Text.Paragraph>
+            <Text.Paragraph size={SizeEnum.H5} color={ColorEnum.BLACK}>124</Text.Paragraph>
+            <Text.Paragraph size={SizeEnum.H6} color={ColorEnum.BLACK}>124</Text.Paragraph>
+
+
+
+
+            <Text.Link to={"124"} size={SizeEnum.H1}>124</Text.Link>
+            <Text.Link to={"124"} size={SizeEnum.H2}>124</Text.Link>
+            <Text.Link to={"124"} size={SizeEnum.H3}>124</Text.Link>
+            <Text.Link to={"124"} size={SizeEnum.H4}>124</Text.Link>
+            <Text.Link to={"124"} size={SizeEnum.H5}>124</Text.Link>
+            <Text.Link to={"124"} size={SizeEnum.H6}>124</Text.Link>
         </div>
     );
 };
